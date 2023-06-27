a44829675e71b760f5817fdad0a4283c627d59a7
WillowSh <jjjjjss123@163.com>
2023-06-27 10:47:28 +0800

    6.27.2

--- src/main/resources/static/pages/answerStats/index.js
+++ src/main/resources/static/pages/answerStats/index.js
@@ -2,7 +2,9 @@ let questionList = [];
 const urlParams = new URLSearchParams(window.location.search);
 const qNRId = urlParams.get('qNRId');
 let ASForStat=[];
-
+let optionContentList=[];
+let answerForStat=[];
+let ratioList=[];
 
 onload = () => {
     let qNRName = '';
@@ -84,106 +86,75 @@ const fetchQuestionList = () => {
         }
     });
 }
-
-const fetchTableInfo =(questionId, tbodyId, buttonId) =>{
+const fetchAnswerStat = async (questionId) => {
     let optionParams = {
         questionId: questionId,
     };
 
-    $.ajax({
+    const optionListRes = await $.ajax({
         url: API_BASE_URL + '/queryOptionList',
         type: 'POST',
         data: JSON.stringify(optionParams),
         dataType: 'json',
         contentType: 'application/json',
-        success(res) {
-            let optionList = [];
-            console.log("success")
-            $(`#${tbodyId}`).html('');
-            optionList = res.data
-            console.log(optionList);
-            res.data.forEach((optionItem, index) => {
-                let answerParams={
-                    answer:optionItem.optionContent,
-                    questionId: questionId,
-                };
-                $.ajax({
-                    url: API_BASE_URL + '/queryAnswerForStat',
-                    type: 'POST',
-                    data: JSON.stringify(answerParams),
-                    dataType: 'json',
-                    contentType: 'application/json',
-                    success(answerRes){
-                        const ratio = (answerRes.data / ASForStat[0]) * 100;
-                        $(`#${tbodyId}`).append(`
-
-                          <tr>
-                            <td>${optionItem.optionContent}</td>
-                            <td>${answerRes.data}</td>
-                            <td id="td">
-                            <p>
-                              <style>
-                                    meter {
-                                        width: 300px; /* 设置进度条的宽度 */
-                                       
-                                        height: 20px;
-                                    }
-                                    meter::-webkit-meter-optimum-value,
-                                    meter::-webkit-meter-suboptimum-value,
-                                    meter::-webkit-meter-even-less-good-value {
-                                        background: #0066ff; /* 设置进度条的优化值、次优化值和更差值的颜色 */
-                                    }
-                                </style>
-                                <meter min="0" max="${ASForStat[0]}" value="${answerRes.data}"></meter>
-                                <span> ${ratio} % </span>
-                            </p>
-                            </td>
-                          </tr>
-                        `)
-                    }
-                })
-
-            })
-        }
     });
-}
+
+    const optionList = optionListRes.data;
+
+    for (let index = 0; index < optionList.length; index++) {
+        const optionItem = optionList[index];
+        let answerParams = {
+            answer: optionItem.optionContent,
+            questionId: questionId,
+        };
+
+        const answerRes = await $.ajax({
+            url: API_BASE_URL + '/queryAnswerForStat',
+            type: 'POST',
+            data: JSON.stringify(answerParams),
+            dataType: 'json',
+            contentType: 'application/json',
+        });
+
+        const ratio = (answerRes.data / ASForStat[0]) * 100;
+        optionContentList[index] = optionItem.optionContent;
+        answerForStat[index] = answerRes.data;
+        ratioList.push(ratio);
+    }
+};
+
 
 const setOriginalInfo = (questionId, tbodyId, buttonId, div1Id, tableId) =>{
     $(`#${buttonId}`).append(`
                          <button onclick="showTable('${div1Id}','${tableId}','${tbodyId}','${buttonId}','${questionId}')">表格</button>
                          <button onclick="showPieChart('${questionId}')">饼状</button>
-                         <button onclick="showBarChart('${questionId}')">柱状</button>
+                         <button onclick="showBarChart('${div1Id}','${buttonId}','${questionId}')">柱状</button>
                         `)
 }
-
-const showTable = (div1Id, tableId, tbodyId, buttonId, id) =>{
+const showBarChart = (div1Id, buttonId, id) =>{
+    fetchAnswerStat(id);
     $(`#${div1Id}`).append(`
-                          <table class="table table-bordered table-striped" id="${tableId}">
-                      <colgroup>
-                        <col style="width: 45%;">
-                        <col style="width: 10%;">
-                        <col style="width: 45%;">
-                      </colgroup>
-                      <thead>
-                       <tr>
-                         <th>选项</th>
-                         <th>小计</th>
-                         <th>比例</th>
-                       </tr>
-                      </thead>
-                      <tbody id="${tbodyId}">
-                
-                
-                      </tbody>
-                      <thead>
-                       <tr>
-                         <th>本题有效填写人次</th>
-                         <th>${ASForStat[0]}</th>
-                         <th> </th>
-                       </tr>
-                      </thead>
-                    </table>
-                        `)
+        <div id="${id}" style="width: 500px;height:400px;"></div>
+    `);
 
-    fetchTableInfo(id, tbodyId, buttonId);
-}
+    // echarts initialization and set options
+    const myChart = echarts.init(document.getElementById(id));
+    const option = {
+        xAxis: {
+            type: 'category',
+            data: optionContentList
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+                data: answerForStat,
+                type: 'bar'
+            }
+        ]
+    };
+
+    myChart.setOption(option);
+};
+
