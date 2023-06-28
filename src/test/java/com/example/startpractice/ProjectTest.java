9d46f41bec9aaa347ffa505b42bccd3b508b93ba
linzhefan <1943465235@qq.com>
2023-06-28 22:48:49 +0800

    test

--- src/test/java/com/example/startpractice/ProjectTest.java
+++ src/test/java/com/example/startpractice/ProjectTest.java
@@ -1,164 +1,103 @@
 package com.example.startpractice;
 
+import com.example.startpractice.beans.HttpResponseEntity;
 import com.example.startpractice.common.utils.UUIDUtil;
+import com.example.startpractice.controller.ProjectController;
 import com.example.startpractice.dao.ProjectEntityMapper;
 import com.example.startpractice.dao.UserEntityMapper;
 import com.example.startpractice.dao.entity.ProjectEntity;
 import com.example.startpractice.dao.entity.UserEntity;
+import jakarta.annotation.Resource;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.apache.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.CollectionUtils;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.Logger;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class ProjectTest {
-
-  //  @Test
-    void contextLoads() {
-    }
-
-    Logger log = Logger.getLogger(ProjectTest.class);
-    @Test
-    public void queryUserList() throws Exception {
-        String resource = "mybatis-config.xml";
-        InputStream inputStream = Resources.getResourceAsStream(resource);
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        //创建UserMapper对象，mybatis自动生成mapper代理对象
-        UserEntityMapper userEntityMapper = sqlSession.getMapper(UserEntityMapper.class);
-        //调用userMapper的方法
-        UserEntity userEntity = new UserEntity();
-        List<UserEntity> list = userEntityMapper.queryUserList(userEntity);
-        if(CollectionUtils.isEmpty(list)){
-            // 记录error级别的信息
-        }else{
-            System.out.println(list);
-            // 记录info级别的信息
-            log.info(">>queryUserList用户列表查询测试成功");
-        }
-    }
-
-    @Test
-    public void selectUserInfo() throws Exception {
-        String resource = "mybatis-config.xml";
-        InputStream inputStream = Resources.getResourceAsStream(resource);
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        //创建UserMapper对象，mybatis自动生成mapper代理对象
-        UserEntityMapper userEntityMapper = sqlSession.getMapper(UserEntityMapper.class);
-        //调用userMapper的方法
-        UserEntity userEntity = new UserEntity();
-        userEntity.setUsername("admin_J");
-        userEntity.setPassword("123");
-        List<UserEntity> list = userEntityMapper.selectUserInfo(userEntity);
-        if(CollectionUtils.isEmpty(list)){
-            // 记录error级别的信息
-        }else{
-            System.out.println(list);
-            // 记录info级别的信息
-            log.info(">>selectUserInfo用户登录测试成功");
-        }
-    }
-
-    @Test
-    public void insert() throws Exception {
-        String resource = "mybatis-config.xml";
-        InputStream inputStream = Resources.getResourceAsStream(resource);
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        //创建UserMapper对象，mybatis自动生成mapper代理对象
-        UserEntityMapper userEntityMapper = sqlSession.getMapper(UserEntityMapper.class);
-        //调用userMapper的方法
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(UUIDUtil.getOneUUID());
-        userEntity.setStatus("1");
-        userEntity.setUsername("LS");
-        userEntity.setPassword("123");
-        int i = userEntityMapper.insert(userEntity);
-        if(i==0){
-            // 记录error级别的信息
-        }else{
-            System.out.println(i);
-            // 记录info级别的信息
-            log.info(">>insert用户插入测试成功");
-        }
-    }
-
-    @Test
-    public void deleteUserById() throws Exception {
-        String resource = "mybatis-config.xml";
-        InputStream inputStream = Resources.getResourceAsStream(resource);
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        //创建UserMapper对象，mybatis自动生成mapper代理对象
-        UserEntityMapper userEntityMapper = sqlSession.getMapper(UserEntityMapper.class);
-        //调用userMapper的方法
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId("1");
-        int i = userEntityMapper.deleteUserById(userEntity);
-        if(i==0){
-            // 记录error级别的信息
-            System.out.println(i);
-        }else{
-            System.out.println(i);
-            // 记录info级别的信息
-            log.info(">>delete用户删除测试成功");
-        }
-    }
+    @Resource
+    private ProjectController projectController;
+    Logger log = Logger.getLogger(String.valueOf(UserTest.class));
     @Test
     public void queryProjectList() throws Exception {
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        //创建ProjectMapper对象，mybatis自动生成mapper代理对象
         ProjectEntityMapper projectEntityMapper = sqlSession.getMapper(ProjectEntityMapper.class);
-        //调用projectMapper的方法
-        ProjectEntity projectEntity = new ProjectEntity();
-        projectEntity.setProjectName("six");
-        List<ProjectEntity> list1 = projectEntityMapper.queryProjectList(projectEntity);
-        projectEntity.setProjectName(null);
-        projectEntity.setCreatedBy("admin_J");
-        List<ProjectEntity> list2 = projectEntityMapper.queryProjectList(projectEntity);
-        if(CollectionUtils.isEmpty(list1)||CollectionUtils.isEmpty(list2)){
-            // 记录error级别的信息
-        }else{
-            System.out.println(list1);
-            // 记录info级别的信息
+
+        /*
+         * 项目查询成功
+         * */
+        ProjectEntity projectEntity_1 = new ProjectEntity();
+        projectEntity_1.setCreatedBy("admin");
+
+        ProjectEntity projectEntity_2 = new ProjectEntity();
+        projectEntity_2.setProjectName("project2");
+
+        /*
+         * 项目查询失败
+         * */
+        ProjectEntity projectEntity_3 = new ProjectEntity();
+        projectEntity_3.setCreatedBy("none");
+
+        ProjectEntity projectEntity_4 = new ProjectEntity();
+        projectEntity_4.setProjectName("noProject");
+
+        HttpResponseEntity httpResponseEntity_1 = projectController.queryProjectList(projectEntity_1);
+        HttpResponseEntity httpResponseEntity_2 = projectController.queryProjectList(projectEntity_2);
+        HttpResponseEntity httpResponseEntity_3 = projectController.queryProjectList(projectEntity_3);
+        HttpResponseEntity httpResponseEntity_4 = projectController.queryProjectList(projectEntity_4);
+
+        if(httpResponseEntity_1.getCode().equals("666") && httpResponseEntity_2.getCode().equals("666")
+                && httpResponseEntity_3.getCode().equals("0") && httpResponseEntity_4.getCode().equals("0"))
+        {
             log.info(">>queryProjectList项目列表查询测试成功");
         }
     }
 
     @Test
-    public void insertProject() throws Exception {
+    public void projectInsert() throws Exception {
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        //创建ProjectMapper对象，mybatis自动生成mapper代理对象
         ProjectEntityMapper projectEntityMapper = sqlSession.getMapper(ProjectEntityMapper.class);
-        //调用projectMapper的方法
+
+        /*
+         * 项目新建成功
+         * */
         ProjectEntity projectEntity = new ProjectEntity();
         projectEntity.setId(UUIDUtil.getOneUUID());
-        projectEntity.setProjectName("much");
-        projectEntity.setProjectContent("test");
-        projectEntity.setCreatedBy("admin_J");
-        int i = projectEntityMapper.insert(projectEntity);
-        if(i==0){
-            // 记录error级别的信息
-        }else{
-            System.out.println(i);
-            // 记录info级别的信息
+        projectEntity.setUserId("1");
+        projectEntity.setProjectName("newProject");
+        projectEntity.setCreatedBy("admin");
+        projectEntity.setLastUpdatedBy("admin");
+        projectEntity.setProjectContent("New Project!");
+
+        /*
+         * 项目新建失败
+         * */
+        /*ProjectEntity projectEntity_2 = new ProjectEntity();
+        projectEntity.setId("none");*/
+
+        HttpResponseEntity httpResponseEntity = projectController.addProjectInfo(projectEntity);
+        /*HttpResponseEntity httpResponseEntity_2 = projectController.addProjectInfo(projectEntity_2);
+         */
+        if(httpResponseEntity.getCode().equals(666))
+        {
             log.info(">>insert项目插入测试成功");
         }
     }
@@ -169,49 +108,66 @@ class ProjectTest {
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        //创建ProjectMapper对象，mybatis自动生成mapper代理对象
         ProjectEntityMapper projectEntityMapper = sqlSession.getMapper(ProjectEntityMapper.class);
-        //调用projectMapper的方法
+
+        /*
+         * 项目删除成功
+         * */
         ProjectEntity projectEntity = new ProjectEntity();
-        projectEntity.setCreatedBy("admin_J");
-        projectEntity.setId("4a00213e1b234adaa04ee952a6655ea0");
-        int i = projectEntityMapper.deleteProjectById(projectEntity);
-        if(i==0){
-            // 记录error级别的信息
-            System.out.println(i);
-        }else{
-            System.out.println(i);
-            // 记录info级别的信息
+        projectEntity.setId("4");
+
+        /*
+         * 项目删除失败
+         * */
+        ProjectEntity projectEntity_2 = new ProjectEntity();
+        projectEntity_2.setId("noId");
+
+        HttpResponseEntity httpResponseEntity = projectController.deleteProjectById(projectEntity);
+        HttpResponseEntity httpResponseEntity_2 = projectController.deleteProjectById(projectEntity_2);
+
+        if(httpResponseEntity.getCode().equals("666") && httpResponseEntity_2.getCode().equals("0"))
+        {
             log.info(">>delete项目删除测试成功");
         }
     }
 
     @Test
-    public void modifyProjectById() throws Exception {
+    public void modifyProjectInfo() throws IOException {
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        //创建ProjectMapper对象，mybatis自动生成mapper代理对象
         ProjectEntityMapper projectEntityMapper = sqlSession.getMapper(ProjectEntityMapper.class);
-        //调用projectMapper的方法
+
         ProjectEntity projectEntity = new ProjectEntity();
-        projectEntity.setId("4a00213e1b234adaa04ee952a6655ea0");
-        projectEntity.setCreatedBy("admin_J");
-        projectEntity.setProjectName("modify");
-        projectEntity.setProjectContent("modified");
-        projectEntity.setLastUpdatedBy("admin_J");
-        int i = projectEntityMapper.modifyProjectInfo(projectEntity);
-        if(i==0){
-            // 记录error级别的信息
-            System.out.println(i);
-        }else{
-            System.out.println(i);
-            // 记录info级别的信息
-            log.info(">>modify项目修改测试成功");
+        /*
+         * 项目修改成功
+         */
+        projectEntity.setId("1");
+        projectEntity.setUserId("1");
+        projectEntity.setCreatedBy("admin");
+        projectEntity.setProjectName("project1_mod");
+        projectEntity.setProjectContent("Project1 is modified!");
+
+        long now = System.currentTimeMillis();
+        Date date = new Date();
+        date.setTime(now);
+        projectEntity.setLastUpdateDate(date);
+        projectEntity.setLastUpdatedBy("admin2");
+
+        /*
+         * 项目修改失败
+         * */
+        ProjectEntity projectEntity_2 = new ProjectEntity();
+        projectEntity_2.setId("noId");
+
+        HttpResponseEntity httpResponseEntity = projectController.modifyProjectInfo(projectEntity);
+        HttpResponseEntity httpResponseEntity_2 = projectController.modifyProjectInfo(projectEntity_2);
+
+        if(httpResponseEntity.getCode().equals("666") && httpResponseEntity_2.getCode().equals("0"))
+        {
+            log.info(">>update项目修改测试成功");
         }
     }
-
-
 
 }
--- src/test/java/com/example/startpractice/UserTest.java
+++ src/test/java/com/example/startpractice/UserTest.java
@@ -1,15 +1,185 @@
 package com.example.startpractice;
 
+import com.example.startpractice.beans.HttpResponseEntity;
+import com.example.startpractice.common.utils.UUIDUtil;
+import com.example.startpractice.controller.UserController;
+import com.example.startpractice.dao.entity.UserEntity;
+import jakarta.annotation.Resource;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Logger;
+
 @SpringBootTest
 public class UserTest {
+    @Resource
+    private UserController userController;
+
+
+    Logger log = Logger.getLogger(String.valueOf(UserTest.class));
     @Test
-    void contextLoads() {
+    public void queryUserList() throws Exception {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        /*
+         **用户存在，查询成功
+         */
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId("1");
+        userEntity.setUsername("admin");
+        /*
+         **用户不存在，查询失败
+         */
+        UserEntity userEntity_2 = new UserEntity();
+        userEntity_2.setId("000");
+        userEntity_2.setUsername("none");
+        /*UserController userController = new UserController();*/
+
+        /*List<UserEntity> list = userEntity.queryUserList(userEntity);*/
+        /*if(CollectionUtils.isEmpty(list)){
+        }else{
+            System.out.println(list);
+            log.info(">>queryUserList用户列表查询测试成功");
+        }*/
+        HttpResponseEntity httpResponseEntity = userController.queryUserList(userEntity);
+        HttpResponseEntity httpResponseEntity_2 = userController.queryUserList(userEntity_2);
+
+        if(httpResponseEntity.getCode().equals("666") && httpResponseEntity_2.getCode().equals(0))
+        {
+            log.info(">>queryUserList用户列表查询测试成功");
+        }
     }
+
     @Test
-    void TestMain(){
-        new StartPracticeApplication().main(new String[]{});
+    public void selectUserInfo() throws Exception {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+
+        /*
+         **用户存在，登录成功
+         */
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId("1");
+        userEntity.setUsername("admin");
+        userEntity.setPassword("111");
+        /*
+         **用户存在，登录失败
+         */
+        UserEntity userEntity_2 = new UserEntity();
+        userEntity_2.setId("000");
+        userEntity_2.setUsername("none");
+
+
+        HttpResponseEntity httpResponseEntity = userController.userLogin(userEntity);
+        HttpResponseEntity httpResponseEntity_2 = userController.userLogin(userEntity_2);
+
+        if(httpResponseEntity.getCode().equals("666") && httpResponseEntity_2.getCode().equals(0))
+        {
+            log.info(">>selectUserInfo用户登录测试成功");
+        }
+    }
+
+    @Test
+    public void insert() throws Exception {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+
+        /*
+         ** 用户信息设置正确，添加成功
+         */
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(UUIDUtil.getOneUUID());
+        userEntity.setStatus("1");
+        userEntity.setUsername("newUser");
+        userEntity.setPassword("123");
+        /*
+         ** 用户信息设置错误，添加失败
+         */
+       /* UserEntity userEntity_2 = new UserEntity();
+        userEntity.setStatus("1");*/
+
+        HttpResponseEntity httpResponseEntity = userController.addUserInfo(userEntity);
+        /*HttpResponseEntity httpResponseEntity_2 = userController.addUser(userEntity_2);
+         */
+        if(httpResponseEntity.getCode().equals("666"))
+        {
+            log.info(">>insert用户插入测试成功");
+        }
+    }
+
+    @Test
+    public void deleteUserById() throws Exception {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+
+        /*
+         * 用户删除成功
+         * */
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId("4");
+        /*
+         * 用户删除失败
+         * */
+        UserEntity userEntity_2 = new UserEntity();
+        userEntity_2.setId("000");
+
+
+        HttpResponseEntity httpResponseEntity = userController.deleteUserById(userEntity);
+        HttpResponseEntity httpResponseEntity_2 = userController.deleteUserById(userEntity_2);
+
+        if(httpResponseEntity.getCode().equals("666") && httpResponseEntity_2.getCode().equals("0"))
+        {
+            log.info(">>delete用户删除测试成功");
+        }
+
+    }
+
+    @Test
+    public void modifyUserId() throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        /*
+         * 用户修改成功
+         * */
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId("2");
+        userEntity.setUsername("admin2");
+        userEntity.setPassword("newPassword");
+        userEntity.setStatus("1");
+        /*
+         * 用户修改失败
+         * */
+        UserEntity userEntity_2 = new UserEntity();
+        userEntity_2.setId("000");
+
+
+        HttpResponseEntity httpResponseEntity = userController.modifyUserInfo(userEntity);
+        HttpResponseEntity httpResponseEntity_2 = userController.modifyUserInfo(userEntity_2);
+
+        if(httpResponseEntity.getCode().equals("666") && httpResponseEntity_2.getCode().equals("0"))
+        {
+            log.info(">>delete用户删除测试成功");
+        }
+
     }
 }
